32568b083afd04a2d2611b6d40aa8578802f2fcf
mountstone <lukasz.cirut@gmail.com>
2017-03-17 13:55:32 +0100

    CHG: assets and views for release 0.9.2

--- public/packages/core/js/on-load.js
+++ public/packages/core/js/on-load.js
@@ -418,15 +418,6 @@ $(document).ready(function () {
         e.preventDefault(), e.stopPropagation(), sidebar.removeClass(openClass);
     });
 
-    $('.users-select-status', document).on('change', function (e) {
-        var table = $(this).closest('.tbl-c').find('[data-table-init]');
-        if (table.length < 0) {
-            return false;
-        }
-        var api = table.dataTable().api();
-        var val = $.fn.dataTable.util.escapeRegex($(this).val());
-        api.column(5).search(val, true, false).draw();
-    });
     $('.admins-select-status', document).on('change', function (e) {
         var table = $(this).closest('.tbl-c').find('[data-table-init]');
         if (table.length < 0) {
