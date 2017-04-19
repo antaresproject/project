b2042a59c42aeae75da51187d126b265ffcdee6d
mountstone <lukasz.cirut@gmail.com>
2017-04-19 16:33:14 +0200

    CHG: quick search engine based on datatables results

--- resources/config/search.php
+++ resources/config/search.php
@@ -1,15 +1,8 @@
 <?php
 
 return [
-    'models'     => [
-        'users'        => \Antares\Model\User::class,
-        'logs'         => \Antares\Logger\Model\LogsTranslations::class,
-        'customfields' => \Antares\Customfields\Model\Field::class,
-        'module'       => Antares\Module\Model\ModuleRow::class
-    ],
     'datatables' => [
         'users'        => \Antares\Users\Http\Datatables\Users::class,
-        'logs'         => \Antares\Logger\Http\Datatables\ActivityLogs::class,
         'customfields' => \Antares\Customfields\Http\Datatables\Customfields::class,
         'module'       => \Antares\Module\Http\Datatables\ModuleDatatable::class
     ]
