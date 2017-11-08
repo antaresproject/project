c7d6dd975584a2a0e4848c657bc7df96b5000cca
mountstone <lukasz.cirut@gmail.com>
2017-11-08 10:36:39 +0100

    ADD: backend integration with new frontend

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,7 +4,6 @@ use Illuminate\Routing\Router;
 
 $router->group(['middleware' => ['antares']], function (Router $router) {
     $router->get('/', 'WelcomeController@index');
-    $router->match(['GET', 'POST'], 'test', 'WelcomeController@test');
 });
 
 
